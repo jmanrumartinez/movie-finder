61682e5b5d053d02668daca3c2330f4e52f325bd
Joan Manrubia <jmanrumartinez@gmail.com>
2020-11-08 19:00:46 +0100

    added overflow & position relative

--- src/modules/Movies/Item/atoms/Details/atoms/DetailWrapper.js
+++ src/modules/Movies/Item/atoms/Details/atoms/DetailWrapper.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 export default styled.div`
+    position: relative; 
     display: none;
-    margin-top: -1px; 
+    margin-top: -45px; 
+    overflow: hidden;
 `;
