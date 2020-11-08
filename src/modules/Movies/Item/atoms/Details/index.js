b5b8e3bfa20a54c84e538929b47cc76642613405
Joan Manrubia <jmanrumartinez@gmail.com>
2020-11-08 18:59:54 +0100

    use data instead of placeholder for testing

--- src/modules/Movies/Item/atoms/Details/index.js
+++ src/modules/Movies/Item/atoms/Details/index.js
@@ -1,14 +1,15 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import Wrapper from './atoms/DetailWrapper';
 import Description from './atoms/DetailDescription';
 import Meta from './atoms/DetailMeta';
 import DetailTitle from './atoms/DetailTitle';
 
-const Details = (props) => (
-  <Wrapper {...props}>
-    <DetailTitle>Mulán</DetailTitle>
-    <Description>Lorem ipsum bla bla bla</Description>
-    <Meta>2020</Meta>
+const Details = ({ data, ...rest }) => (
+  <Wrapper {...rest}>
+    <DetailTitle>{data.title}</DetailTitle>
+    <Description>{data.overview}</Description>
+    <Meta>{data.release_date}</Meta>
   </Wrapper>
 );
 
