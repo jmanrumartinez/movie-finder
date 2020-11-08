9703fbbaccbe40689419aadcfb449b8d7fcaabee
Joan Manrubia <jmanrumartinez@gmail.com>
2020-11-08 19:55:56 +0100

    implemented useSearchMovie custom hook

--- src/modules/Hooks/useSearchMovie.js
+++ src/modules/Hooks/useSearchMovie.js
@@ -1,15 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect, useReducer } from 'react';
+import ApiClient from '../../utils/ApiClient';
+import PromiseEnum from '../../utils/enums/PromiseEnum';
+import AsyncReducer from '../common/Reducers/AsyncReducer';
 
 const useSearchMovie = (value) => {
-  if (!value) return;
+  const [state, dispatch] = useReducer(AsyncReducer, {});
+
   useEffect(() => {
+    if (!value) return '';
     const timeout = setTimeout(() => {
-      console.log(value);
-      // API SEARCH
+      ApiClient.get('search/movie', {
+        language: 'es',
+        query: value,
+      }).then((data) => {
+        dispatch({ type: PromiseEnum.Resolved, data });
+      }).catch((error) => {
+        dispatch({ type: PromiseEnum.Rejected, error });
+      });
     }, 1000);
 
     return () => clearTimeout(timeout);
   }, [value]);
+
+  return { ...state };
 };
 
 export default useSearchMovie;
