597b3a21cbbab0d98bf51a51116f82fd87ba39e9
Joan Manrubia <jmanrumartinez@gmail.com>
2020-11-08 19:01:14 +0100

    use incoming expand prop instead of hover

--- src/modules/Movies/List/atoms/ListWrapper.js
+++ src/modules/Movies/List/atoms/ListWrapper.js
@@ -2,14 +2,15 @@ import styled from 'styled-components';
 
 export default styled.div`
     width: 100%; 
+    padding: 10px; 
     overflow-x: auto; 
     overflow-y: visible;
     -ms-overflow-style: none; 
     scrollbar-width: none;
     box-sizing: border-box;
-    &:hover{
-        margin-bottom: -101px;
-    }
+    position: relative; 
+    margin-bottom: ${(props) => (props.expand ? '-119.44px' : '0')};
+    z-index: ${(props) => (props.expand ? '5' : '1')}; 
    &::-webkit-scrollbar {
         display: none;
     }
