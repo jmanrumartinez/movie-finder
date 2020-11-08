eb31a1fe7e87b504b0a2a83731066514751a2160
Joan Manrubia <jmanrumartinez@gmail.com>
2020-11-08 18:57:13 +0100

    use customLists instead of Item placeholders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import Container from './modules/common/Container';
+import { SearchValueProvider } from './modules/common/Context/SearchValueContext';
 import GlobalStyle from './modules/common/GlobalStyle';
 import NavBar from './modules/common/NavBar';
-import Item from './modules/Movies/Item';
-import List from './modules/Movies/List';
+import useSearchMovie from './modules/Hooks/useSearchMovie';
+import ComedyList from './modules/Movies/ComedyList';
+import TerrorList from './modules/Movies/TerrorList';
+import FantasyList from './modules/Movies/FantasyList';
+import AdventureList from './modules/Movies/AdventureList';
 
 const mainTheme = {
   mainColor: '#1a252f',
@@ -12,29 +16,21 @@ const mainTheme = {
 };
 
 function App() {
+  const [searchValue, setSearchValue] = useState();
+  useSearchMovie(searchValue);
+
   return (
     <ThemeProvider theme={mainTheme}>
-      <GlobalStyle />
-      <NavBar />
-      <Container>
-        <List title="Novedades">
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-        </List>
-        <List title="Novedades">
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-        </List>
-      </Container>
+      <SearchValueProvider value={[searchValue, setSearchValue]}>
+        <GlobalStyle />
+        <NavBar />
+        <Container>
+          <TerrorList />
+          <ComedyList />
+          <FantasyList />
+          <AdventureList />
+        </Container>
+      </SearchValueProvider>
     </ThemeProvider>
   );
 }
