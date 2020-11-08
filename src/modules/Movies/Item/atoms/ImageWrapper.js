32f3393e69ebfe8dd94e43daccd776b37632c5f2
Joan Manrubia <jmanrumartinez@gmail.com>
2020-11-08 12:35:55 +0100

    created ImageWrapper atom for Item

---
+++ src/modules/Movies/Item/atoms/ImageWrapper.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export default styled.div`
+    height: 180px; 
+    width: 100%; 
+    position: relative; 
+    overflow: hidden; 
+    z-index: -1; 
+`;
