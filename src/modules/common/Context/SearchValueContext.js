b26d6b19c9ebeba8eb89c26c2963ba4c93024ca6
Joan Manrubia <jmanrumartinez@gmail.com>
2020-11-08 19:20:13 +0100

    added propTypes for SearchValueContext

--- src/modules/common/Context/SearchValueContext.js
+++ src/modules/common/Context/SearchValueContext.js
@@ -1,10 +1,17 @@
 import React, { createContext } from 'react';
+import PropTypes from 'prop-types';
 
 export const SearchValueContext = createContext();
 
-// eslint-disable-next-line react/prop-types
 export const SearchValueProvider = ({ value, children }) => (
-  <SearchValueContext.Provider value={value}>
+  <SearchValueContext.Provider value={[value.searchValue, value.setSearchValue]}>
     {children}
   </SearchValueContext.Provider>
 );
+
+SearchValueProvider.propTypes = {
+  value: PropTypes.shape({
+    searchValue: PropTypes.string,
+    setSearchValue: PropTypes.func.isRequired,
+  }).isRequired,
+};
