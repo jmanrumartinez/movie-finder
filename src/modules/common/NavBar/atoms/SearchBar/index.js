2c2db3a2b8f67bfd649372237b291cecd36f4d65
Joan Manrubia <jmanrumartinez@gmail.com>
2020-11-09 13:31:41 +0100

    refactored doble useStates to useReducer to improve re-rendering perfomance

--- src/modules/common/NavBar/atoms/SearchBar/index.js
+++ src/modules/common/NavBar/atoms/SearchBar/index.js
@@ -1,26 +1,39 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useReducer } from 'react';
 import { SearchValueContext } from '../../../Context/SearchValueContext';
 import Icon from './atoms/Icon';
 import Input from './atoms/Input';
 import Wrapper from './atoms/Wrapper';
 
+const searchBarReducer = (state, action) => {
+  switch (action.type) {
+    case 'CLICK_ICON':
+      return { isOpen: true, animate: true };
+    case 'ON_BLUR':
+      return { ...state, isOpen: false };
+    default:
+      return state;
+  }
+};
+
 const SearchBar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [animate, setAnimate] = useState(false);
+  const [state, dispatch] = useReducer(searchBarReducer, {
+    isOpen: false,
+    animate: false,
+  });
   const [searchValue, setSearchValue] = useContext(SearchValueContext);
+  const { isOpen, animate } = state;
 
   return (
     <Wrapper isOpen={isOpen} animate={animate}>
       <Icon onClick={() => {
-        setIsOpen(true);
-        setAnimate(true);
+        dispatch({ type: 'CLICK_ICON' });
       }}
       />
       {isOpen && (
       <Input
         type="search"
         placeholder="Buscar"
-        onBlur={() => setIsOpen(false)}
+        onBlur={() => dispatch({ type: 'ON_BLUR' })}
         autoFocus
         value={searchValue}
         onChange={(e) => setSearchValue(e.target.value)}
